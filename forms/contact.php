fb44072fb76775b64bc553a6ce9093ec877385e8
bonganes <bonganealfred@gmail.com>
2022-10-19 12:53:24 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,44 +1,29 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'bonganealfred@gmail.com';
-
-  <script>console_log('bongane here 1');</script>
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  echo 'Bongane here';
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'bonganealfred@gmail',
-    'password' => 'Tshepang@1',
-    'port' => '587'
-  );
- */
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+  if(isset($_POST["submit"])){
+    // Checking For Blank Fields..
+    if($_POST["name"]==""||$_POST["email"]==""||$_POST["subject"]==""||$_POST["message"]==""){
+      echo "Fill All Fields..";
+    }else{
+      // Check if the "Sender's Email" input field is filled out
+      $email=$_POST['email'];
+      // Sanitize E-mail Address
+      $email =filter_var($email, FILTER_SANITIZE_EMAIL);
+      // Validate E-mail Address
+      $email= filter_var($email, FILTER_VALIDATE_EMAIL);
+      if (!$email){
+        echo "Invalid Sender's Email";
+      }
+      else{
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        $headers = 'From:'. $email2 . "rn"; // Sender's Email
+        $headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
+        // Message lines should not exceed 70 characters (PHP rule), so wrap it
+        $message = wordwrap($message, 70);
+        // Send Mail By PHP Mail Function
+        mail("recievers_mail_id@xyz.com", $subject, $message, $headers);
+        echo "Your mail has been sent successfuly ! Thank you for your feedback";
+       }
+    }
+}
 ?>
