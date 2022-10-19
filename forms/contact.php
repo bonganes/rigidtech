89ae1e084ccd5bb2a27dedf5af0d8ebf522f40b3
bonganes <bonganealfred@gmail.com>
2022-10-19 11:40:29 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -24,13 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'bonganealfred@gmail',
     'password' => 'Tshepang@1',
     'port' => '587'
   );
-
+ */
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
