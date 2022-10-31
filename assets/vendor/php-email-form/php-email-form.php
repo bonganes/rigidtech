0da9d983ad53166692bb3fc2d9b74de6cbc14cbd
bonganes <bonganealfred@gmail.com>
2022-10-31 16:41:31 +0200

    Update php-email-form.php

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -5,13 +5,5 @@
      public $from_email;
      public $subject; 
      private $message;
-  
-     function add_message($msg,$wordwrap){
-        $this->message = wordwrap($msg,$wordwrap);
-     }
-  
-     function send(){
-	 mail($to,$subject,$message); 
-     }  
 }
 ?>
