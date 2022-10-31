550c6444d709dcc648e387481f1afdbebd6eb81d
bonganes <bonganealfred@gmail.com>
2022-10-31 18:48:02 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,14 @@
 <?php
-   $to_address = 'bonganealfred@gmail.com';
-   $from_name = $_POST['name'];
-   $from_email = $_POST['email'];
-   $subject = $_POST['subject'];
-   $message = $_POST['message'];
+
+  function sendMail(){
+    $to_address = 'bonganealfred@gmail.com';
+    $from_name = $_POST['name'];
+    $from_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
       
-   echo mail($to_address,$subject, $message);
+   echo "OK";
+  }
+
+  sendMail(); 
 ?>
