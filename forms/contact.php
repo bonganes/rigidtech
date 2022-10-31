9d2bb772577d362cb95a47752d69ee79460cec06
bonganes <bonganealfred@gmail.com>
2022-10-31 17:06:00 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,3 @@
 <?php
-  class PHP_Email_Form{
-     public $to;
-     public $from_name;
-	   public $from_email;
-	   public $subject;
-	   private $message;
-  }
-  
-  $receiving_email_address = 'rigidtech2@gmail.com';
-
-  $contact = new PHP_Email_Form();
-
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+   echo "Successfully came in here";
 ?>
