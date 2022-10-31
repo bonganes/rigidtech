09cdaab6baa3a613a11b32e7f186d5f6a66aeca6
bonganes <bonganealfred@gmail.com>
2022-10-31 16:50:58 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,14 +1,16 @@
 <?php
+  class PHP_Email_Form{
+     public $to;
+     public $from_name;
+	   public $from_email;
+	   public $subject;
+	   private $message;
+  }
+  
   $receiving_email_address = 'rigidtech2@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
   $contact = new PHP_Email_Form();
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
