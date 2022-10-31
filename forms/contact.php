66f67fadad5ec988f39551276d77c6bbae58d844
bonganes <bonganealfred@gmail.com>
2022-10-31 15:06:31 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,29 +1,58 @@
-<?php
-  if(isset($_POST["submit"])){
-    // Checking For Blank Fields..
-    if($_POST["name"]==""||$_POST["email"]==""||$_POST["subject"]==""||$_POST["message"]==""){
-      echo "Fill All Fields..";
-    }else{
-      // Check if the "Sender's Email" input field is filled out
-      $email=$_POST['email'];
-      // Sanitize E-mail Address
-      $email =filter_var($email, FILTER_SANITIZE_EMAIL);
-      // Validate E-mail Address
-      $email= filter_var($email, FILTER_VALIDATE_EMAIL);
-      if (!$email){
-        echo "Invalid Sender's Email";
-      }
-      else{
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
-        $headers = 'From:'. $email2 . "rn"; // Sender's Email
-        $headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
-        // Message lines should not exceed 70 characters (PHP rule), so wrap it
-        $message = wordwrap($message, 70);
-        // Send Mail By PHP Mail Function
-        mail("recievers_mail_id@xyz.com", $subject, $message, $headers);
-        echo "Your mail has been sent successfuly ! Thank you for your feedback";
-       }
-    }
+<?php 
+$toEmail = 'bonganealfred@gmail.com'; 
+$fromName = 'Sender Name'; 
+$formEmail = 'sender@example.com'; 
+ 
+$postData = $statusMsg = $valErr = ''; 
+$status = 'error'; 
+ 
+// If the form is submitted 
+if(isset($_POST['submit'])){ 
+    // Get the submitted form data 
+    $postData = $_POST; 
+    $name = trim($_POST['name']); 
+    $email = trim($_POST['email']); 
+    $subject = trim($_POST['subject']); 
+    $message = trim($_POST['message']); 
+     
+    // Validate form fields 
+    if(empty($name)){ 
+         $valErr .= 'Please enter your name.<br/>'; 
+    } 
+    if(empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL) === false){ 
+        $valErr .= 'Please enter a valid email.<br/>'; 
+    } 
+    if(empty($subject)){ 
+        $valErr .= 'Please enter subject.<br/>'; 
+    } 
+    if(empty($message)){ 
+        $valErr .= 'Please enter your message.<br/>'; 
+    } 
+     
+    if(empty($valErr)){ 
+        // Send email notification to the site admin 
+        $subject = 'New contact request submitted'; 
+        $htmlContent = " 
+            <h2>Contact Request Details</h2> 
+            <p><b>Name: </b>".$name."</p> 
+            <p><b>Email: </b>".$email."</p> 
+            <p><b>Subject: </b>".$subject."</p> 
+            <p><b>Message: </b>".$message."</p> 
+        "; 
+         
+        // Always set content-type when sending HTML email 
+        $headers = "MIME-Version: 1.0" . "\r\n"; 
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+        // Header for sender info 
+        $headers .= 'From:'.$fromName.' <'.$formEmail.'>' . "\r\n"; 
+         
+        // Send email 
+        @mail($toEmail, $subject, $htmlContent, $headers); 
+         
+        $status = 'success'; 
+        $statusMsg = 'Thank you! Your contact request has submitted successfully, we will get back to you soon.'; 
+        $postData = ''; 
+    }else{ 
+        $statusMsg = '<p>Please fill all the mandatory fields:</p>'.trim($valErr, '<br/>'); 
+    } 
 }
-?>
